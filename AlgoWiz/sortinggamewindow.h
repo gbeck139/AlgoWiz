e8d39b0072a02a6a3d588c51be0e1d4986f11dd4
Josh (Orangers) Eggett <joshuaoeggett@gmail.com>
2025-04-24 15:24:45 -0600

    Document sortinggamewindow.h

--- AlgoWiz/sortinggamewindow.h
+++ AlgoWiz/sortinggamewindow.h
@@ -13,25 +13,80 @@ class SortingGameWindow : public QWidget
 {
     Q_OBJECT
 public:
+    /**
+     * @brief SortingGameWindow - constructs a sorting game window.
+     * @param parent - The parent widget
+     */
     explicit SortingGameWindow(QWidget *parent = nullptr);
 
 signals:
+    /**
+     * @brief difficultySelected - signal to be emitted when a difficulty is selected.
+     * @param difficulty - the difficulty to select.
+     */
     void difficultySelected(int difficulty);
 
 private slots:
+    /**
+     * @brief handleDifficultySelection - handles logic for setting difficulty and swapping to respective algorithm.
+     * @param difficulty - what difficulty was selected (0 = easy, 1 = normal, anything else = hard)
+     */
     void handleDifficultySelection(int difficulty);
+
+    /**
+     * @brief checkAIProgress - helper that checks whether the ai algorithm has finished sorting or not.
+     */
     void checkAIProgress();
+
+    /**
+     * @brief handlePlayerFinished - slot that handles logic for player finishing the game.
+     */
     void handlePlayerFinished();
 
 private:
+    /**
+     * @brief layout - vertical layout to organize the widget components of this widget game.
+     */
     QVBoxLayout *layout;
+
+    /**
+     * @brief easySelection - button to select easy difficulty (v.s. insertion sort)
+     */
     QPushButton *easySelection;
+
+    /**
+     * @brief mediumSelection - button to select normal difficulty (v.s. merge sort)
+     */
     QPushButton *mediumSelection;
+
+    /**
+     * @brief hardSelection - button to select hard difficulty (v.s. stalin sort)
+     */
     QPushButton *hardSelection;
+
+    /**
+     * @brief aiPlayer - the widget for the ai player.
+     */
     sortingAlgoRenderer *aiPlayer;
+
+    /**
+     * @brief playerController - the widget that the player uses to sort the bars.
+     */
     sortingGamePlayer *playerController;
+
+    /**
+     * @brief gameResultLabel - label to display the result of the game (whether ai or player won).
+     */
     QLabel *gameResultLabel;
+
+    /**
+     * @brief aiFinished - whether the ai sorting algorithm has finished or not.
+     */
     bool aiFinished = false;
+
+    /**
+     * @brief playerFinished - whether the player has finished or not.
+     */
     bool playerFinished = false;
 };
 
