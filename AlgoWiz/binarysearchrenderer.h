a2345a937f0d5af27492c4100091dc72f947b2ef
leewo <leewo@WOOJIN>
2025-04-24 23:50:27 -0600

    Add comments for binarySearch and quiz

--- AlgoWiz/binarysearchrenderer.h
+++ AlgoWiz/binarysearchrenderer.h
@@ -1,3 +1,10 @@
+/**
+ * @file binarysearchrenderer.h
+ * @author Woojin Lee, David Perry
+ * @brief This file contains the declaration of the BinarySearchRenderer class to create the window for binarysearch algorithm.
+ * @date 2025-04-24
+ */
+
 #ifndef BINARYSEARCHRENDERER_H
 #define BINARYSEARCHRENDERER_H
 
@@ -6,14 +13,40 @@
 class BinarySearchRenderer : public sortingAlgoRenderer
 {
 public:
+    /**
+     * @brief BinarySearchRenderer - constructs a binary search renderer and starts the animation.
+     */
     BinarySearchRenderer();
 
 private:
+    /**
+     * @brief low - The left boundary of the current search interval.
+     */
     int low = 0;
+
+    /**
+     * @brief high - The right boundary of the current search interval.
+     */
     int high = 0;
+
+    /**
+     * @brief target - The target value to search for.
+     */
     int target = 0;
+
+    /**
+     * @brief stepInProgress - Check whether the binary serving algorithm is currently in progress.
+     */
     bool stepInProgress = false;
+
+    /**
+     * @brief runSearchStep - helper to merge and handle the timing and finish logic after queuing merge steps.s
+     */
     void runSearchStep();
+
+    /**
+     * @brief startAnimation - starts the animation and queues steps.
+     */
     void startAnimation();
 };
 
--- AlgoWiz/binarysearchwindow.h
+++ AlgoWiz/binarysearchwindow.h
@@ -1,17 +1,29 @@
+/**
+ * @file binarysearchwindow.h
+ * @author Woojin Lee, David Perry
+ * @brief This file contains the declaration of the BinarySearchWindow class to create the window for binarysearch algorithm.
+ * @date 2025-04-24
+ */
+
 #ifndef BINARYSEARCHWINDOW_H
 #define BINARYSEARCHWINDOW_H
 
 #include <QWidget>
-//#include <QPushButton>
 #include "binarysearchrenderer.h"
-//#include "quizwindow.h"
 
 class BinarySearchWindow : public QWidget
 {
 public:
+    /**
+     * @brief BinarySearchWindow - constructs a BinarySearchWindow
+     * @param parent The parent widget
+     */
     BinarySearchWindow(QWidget *parent = nullptr);
 
 private:
+    /**
+     * @brief renderer - the BinarySearchRenderer to display the binary search animation and starts it automatically.
+     */
     BinarySearchRenderer *renderer;
 };
 
